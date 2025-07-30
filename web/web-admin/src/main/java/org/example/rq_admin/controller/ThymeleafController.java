dae5dd881b5fe627baffed9a2809e68f3613e08c
Johnifirst <a15708295047@outlook.com>
2025-07-30 16:09:00 +0800

    feat: 增加thymeleaf表单交互示例

--- web/web-admin/src/main/java/org/example/rq_admin/controller/ThymeleafController.java
+++ web/web-admin/src/main/java/org/example/rq_admin/controller/ThymeleafController.java
@@ -1,17 +1,28 @@
 package org.example.rq_admin.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.example.rq_admin.entity.ThymeleafFormTest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "thymeleaf样例", description = "thymeleaf页面测试")
 @Controller
-@ResponseBody
 public class ThymeleafController {
 
     @GetMapping("/thymeleaf")
     public String thymeleaf() {
         return "thymeleaf";
+    }
+
+    @PostMapping("/thymeleaf/form")
+    public String thymeleafForm(
+            ThymeleafFormTest thymeleafFormTest,
+            Model model
+    ) {
+        model.addAttribute("user", thymeleafFormTest);
+        return "submit-success";
     }
 }
---
+++ web/web-admin/src/main/java/org/example/rq_admin/entity/ThymeleafFormTest.java
@@ -0,0 +1,15 @@
+package org.example.rq_admin.entity;
+
+import lombok.Data;
+
+@Data
+public class ThymeleafFormTest {
+
+    public String name;
+
+    public int age;
+
+    public String email;
+
+    public String remarks;
+}
