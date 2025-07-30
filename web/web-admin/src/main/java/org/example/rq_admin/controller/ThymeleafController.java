afbdfa18a412204ecbc6c2ba33156cfb86348939
Johnifirst <a15708295047@outlook.com>
2025-07-30 16:09:10 +0800

    feat: 增加thymeleaf表单交互示例

--- web/web-admin/src/main/java/org/example/rq_admin/controller/ThymeleafController.java
+++ web/web-admin/src/main/java/org/example/rq_admin/controller/ThymeleafController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "thymeleaf样例", description = "thymeleaf页面测试")
 @Controller
