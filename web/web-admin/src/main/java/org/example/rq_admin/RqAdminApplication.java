f4fff45ab28f066a0709f9f98538acc95257604c
Johnifirst <a15708295047@outlook.com>
2025-08-06 21:54:47 +0800

    feat: 配置开启事务管理

--- web/web-admin/src/main/java/org/example/rq_admin/RqAdminApplication.java
+++ web/web-admin/src/main/java/org/example/rq_admin/RqAdminApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+@EnableTransactionManagement // 开启自动事务管理
 @SpringBootApplication
 public class RqAdminApplication {
 
@@ -19,8 +21,8 @@ public class RqAdminApplication {
         String port = env.getProperty("server.port");
         System.out.println("\n----------------------------------------------------------\n\t" +
                 "Application rq-admin is running! Access URLs:\n\t" +
-                "Local: \t\thttp://localhost:" + port  + "/\n\t" +
-                "External: \thttp://" + ip + ":" + port  + "/\n\t" +
+                "Local: \t\thttp://localhost:" + port + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + "/\n\t" +
                 "Swagger文档: \thttp://" + ip + ":" + port + "/doc.html\n" +
                 "----------------------------------------------------------");
     }
--- web/web-admin/src/main/java/org/example/rq_admin/controller/ResponseByRequestData.java
+++ web/web-admin/src/main/java/org/example/rq_admin/controller/ResponseByRequestData.java
@@ -3,25 +3,26 @@ package org.example.rq_admin.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.example.rq_admin.enums.ResponseStatus;
 import org.example.rq_admin.response_format.FormatResponseData;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "根据参数返回状态", description = "测试接口，根据请求参数返回接口请求成功还是失败")
+@Slf4j
 @RestController
 @RequestMapping
 public class ResponseByRequestData {
-    private static final Logger logger = LoggerFactory.getLogger(ResponseByRequestData.class);
-
 
     @Operation(summary = "状态相应", description = "根据请求参数，接口返回处理成功/失败")
     @GetMapping("/test/response-by-params/{isSuccess}")
     public FormatResponseData ResponseByRequestDataFn(
             @Parameter(description = "期待响应成功还是失败") @PathVariable("isSuccess") Boolean isSuccess
     ) {
-        logger.info("测试接口接收到了测试请求，此次请求" + isSuccess.toString());
+        log.info("测试接口接收到了测试请求，此次请求{}", isSuccess.toString());
         return new FormatResponseData<>(isSuccess ? ResponseStatus.SUCCESS : ResponseStatus.FAILURE);
     }
 }
